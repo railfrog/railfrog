5414cb0251b4c69a975287894aa573d7e52a061f
Thomas Nichols <thomas@nexus10.com>
2009-06-26 20:32:19 +0100

    Dropping reference_application (now in railfrogcms repo).

--- reference_application/db/migrate/001_railfrog_to_version_6.rb
+++
@@ -1,9 +0,0 @@
-class RailfrogToVersion6 < ActiveRecord::Migration
-  def self.up
-    Engines.plugins["railfrog"].migrate(6)
-  end
-
-  def self.down
-    Engines.plugins["railfrog"].migrate(0)
-  end
-end
